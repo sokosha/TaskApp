22dd316cf58eb4a047a68b026e339d0c5f50d152
EC2 Default User <ec2-user@ip-172-31-28-26.ap-northeast-1.compute.internal>
2020-12-09 00:16:29 +0000

    タスク新規登録修正12/09

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,26 @@
 class TasksController < ApplicationController
+  
   def index
+    @tasks = Task.all
+  end
+  
+  def new
+    @user = User.find_by(params[:user_id])
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(
+      name: params[:name],
+      description: params[:description]
+      )
+    @task.save
+        flash[:notice] = "タスクを新規登録しました"
+    redirect_to user_tasks_path
+  end
+  
+
+  def show
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
 
   root 'static_pages#top'
   get '/signup', to: 'users#new'
@@ -8,5 +7,7 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  resources :users
+  resources :users do
+  resources :tasks
+  end
 end
