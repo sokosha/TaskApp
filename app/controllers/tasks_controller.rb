a92fd1a9ef90f6043321a12c1af2230d8adcf9aa
EC2 Default User <ec2-user@ip-172-31-28-26.ap-northeast-1.compute.internal>
2020-12-10 00:37:37 +0000

    新規登録 C6終了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,10 +10,10 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(
-      name: params[:name],
-      description: params[:description]
-      )
+    @user = User.find_by(params[:user_id])
+    @task = @user.tasks.new(task_params)
+    # @task = @user.tasks.new(neme: params[:task][:name], description: params[:task][:description])
+    # @task = Task.new(neme: params[:task][:name], description: params[:task][:description], user_id: @user.id)
     @task.save
         flash[:notice] = "タスクを新規登録しました"
     redirect_to user_tasks_path
@@ -23,4 +23,9 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+  
+  private
+    def task_params
+        params.require(:task).permit(:name, :description)
+    end
 end
